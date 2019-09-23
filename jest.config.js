168123d0c3f70fd7a1d711150ef552d9ed0ad87b
Jarom Loveridge <jaroml@gradecam.com>
2019-09-23 11:00:47 -0600

    Tests must have `spec.` or `test.` in their name.

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testMatch: [ '**/?(*.)+(spec|test).[jt]s?(x)' ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
